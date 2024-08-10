3d3616173a7593e0e0272eacdadee100dd106086
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-08-10 14:20:33 -0300

    feat/task/creation of the delete and find all routine

--- src/resources/task/task.controller.ts
+++ src/resources/task/task.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body, Param, Get, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from '@prisma/client';
 import { TaskService } from './task.service';
@@ -21,5 +30,21 @@ export class TaskController {
   @Patch('/:id')
   async update(@Param('id') id: string, @Body() body: UpdateTaskDto) {
     return await this.taskService.update(+id, body);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id: string) {
+    return await this.taskService.delete(+id);
+  }
+
+  @Get()
+  async findAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+
+    return await this.taskService.findAll(pageNumber, limitNumber);
   }
 }
--- src/resources/task/task.service.ts
+++ src/resources/task/task.service.ts
@@ -30,6 +30,25 @@ export class TaskService {
     return task;
   }
 
+  async findAll(page: number = 1, limit: number = 10) {
+    const [tasks, total] = await this.prisma.$transaction([
+      this.prisma.task.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      this.prisma.task.count(),
+    ]);
+
+    const totalPages = Math.ceil(total / limit);
+
+    return {
+      tasks,
+      total,
+      totalPages,
+      currentPage: page,
+    };
+  }
+
   async update(id: number, body: UpdateTaskDto): Promise<Task> {
     const task = await this.prisma.task.findUnique({
       where: { id },
@@ -44,5 +63,15 @@ export class TaskService {
     });
 
     return updateTask;
+  }
+
+  async delete(id: number): Promise<void> {
+    try {
+      await this.prisma.task.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw new HttpException('Tarefa não encontrada', HttpStatus.NOT_FOUND);
+    }
   }
 }
